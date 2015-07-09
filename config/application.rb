5060f93040ede7c86a7dc2ea853458285ba97c84
Preston Copeland <copelandps1@gmail.com>
2015-07-09 09:30:19 -0400

    remove Dir glob in config application file

--- config/application.rb
+++ config/application.rb
@@ -12,9 +12,7 @@ module Juniordev
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     # add all asset pipeline images sub folders
-    Dir.glob("#{Rails.root}/app/assets/images/**/").each do |path|
-      config.assets.paths << path
-    end
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'

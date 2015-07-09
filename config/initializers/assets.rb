ddd716be2a28d26187aed895077c1d2dc6bfb04f
Preston Copeland <copelandps1@gmail.com>
2015-07-08 20:34:02 -0400

    change assets config

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -16,18 +16,3 @@ Rails.application.config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif,
                                  "fontawesome-webfont.woff")
 
 
-Rails.application.config.assets.precompile << Proc.new do |path|
-      if path =~ /\.(css|js)\z/
-        full_path = Rails.application.assets.resolve(path).to_path
-        app_assets_path = Rails.root.join('app', 'assets').to_path
-        if full_path.starts_with? app_assets_path
-          puts "including asset: " + full_path
-          true
-        else
-          puts "excluding asset: " + full_path
-          false
-        end
-      else
-        false
-      end
-    end

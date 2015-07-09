eb3ba31ca62da23c9c263945607859b9dd981be5
Preston Copeland <copelandps1@gmail.com>
2015-07-08 20:07:38 -0400

    add config application

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+config.assets.paths << "#{Rails}/vendor/assets/fonts"
+
 module Juniordev
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
